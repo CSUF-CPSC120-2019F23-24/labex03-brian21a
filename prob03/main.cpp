9eaab4a3769979484217ac222ceecba96157b00c
brian21a <Sourcream@csu.fullerton.edu>
2019-09-14 16:33:31 -0700

    displaying height in feet and inches

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,18 @@
 // This program calculates a person's height in feet (') and inches (").
+
+#include<iostream>
+int main()
+{
+int feet;
+int inches;
+int remainder;
+
+
+
+std::cout << "What is your height in inches?";
+std::cin >> inches;
+feet = inches/12;
+remainder = inches%12;
+std::cout << "That person is " << feet << "\'" << remainder << "\"" << inches << "\"";
+  return 0;
+}
